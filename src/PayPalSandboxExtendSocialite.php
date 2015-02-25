93e4b7fef015ef2b7bfafc95bd905204c20c596b
DraperStudio <hello@draperstud.io>
2015-02-25 11:54:17 +0100

    Big clean up

--- src/PayPalSandboxExtendSocialite.php
+++ src/PayPalSandboxExtendSocialite.php
@@ -5,8 +5,13 @@ use SocialiteProviders\Manager\SocialiteWasCalled;
 
 class PayPalSandboxExtendSocialite
 {
+    /**
+     * Execute the provider.
+     */
     public function handle(SocialiteWasCalled $socialiteWasCalled)
     {
-        $socialiteWasCalled->extendSocialite('paypal_sandbox', __NAMESPACE__.'\Provider');
+        $socialiteWasCalled->extendSocialite(
+            'paypal_sandbox', __NAMESPACE__.'\Provider'
+        );
     }
 }
